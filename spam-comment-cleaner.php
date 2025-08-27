6524cef6f20d60b783b25a3c142806a7bfa1fe37
Ivan Eguiguren <donosor00@gmail.com>
2025-08-27 13:08:12 +0200

    Bump version to 1.0.1

--- spam-comment-cleaner.php
+++ spam-comment-cleaner.php
@@ -19,7 +19,7 @@ define('SCC_PLUGIN_FILE', __FILE__);
 define('SCC_PLUGIN_BASENAME', plugin_basename(__FILE__));
 define('SCC_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('SCC_PLUGIN_URL', plugin_dir_url(__FILE__));
-define('SCC_PLUGIN_VERSION', '1.0.0');
+define('SCC_PLUGIN_VERSION', '1.0.1');
 define('SCC_GITHUB_REPO', 'donosor00/spam-comment-cleaner');
 
 class SpamCommentCleaner {
